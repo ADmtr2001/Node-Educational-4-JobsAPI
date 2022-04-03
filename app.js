19201a68bab0b8e7725def7516ab75dde78dd18a
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-04-03 19:44:58 +0300

    add security middleware

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+}));
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
@@ -34,14 +42,14 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
